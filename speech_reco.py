e960cf7dda508771675d832dc0ecbb382098271d
Hrishi <hrishikeshmane87933@gmail.com>
2018-09-07 19:09:50 +0530

    try-except block added

--- speech_reco.py
+++ speech_reco.py
@@ -5,6 +5,8 @@ reco=sp.Recognizer()
 with sp.Microphone() as src:
 	print("Listening....")
 	audio=reco.listen(src)
-
-text=reco.recognize_google(audio)
+try:
+	text=reco.recognize_google(audio)
+except:
+	print("exception caught!")
 print(text)
